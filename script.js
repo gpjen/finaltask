b7532c23b067b25008123d60f520df471f78a16c
agan95 <nyonggpj9995@gmail.com>
2022-06-27 20:18:59 +0900

    fix content scroll snap

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // nav active handler
+const showNavMenu = document.getElementById("isShow");
+const navBrand = document.getElementsByClassName("navBrand");
+const navMenu = document.querySelector(".navMenu");
+const btnMenu = document.querySelectorAll(".btnMenu a");
+const containerContent = document.querySelector("main");
+const content = document.querySelectorAll(".content");
+
+navMenu.addEventListener("click", (e) => {
+  btnMenu.forEach((x) => {
+    x.className = "btnMenu";
+  });
+  e.target.classList.toggle("active");
+});
+
+showNavMenu.addEventListener("click", (e) => {
+  navMenu.classList.toggle("showNavOnMobile");
+});
+
+containerContent.addEventListener("scroll", () => {});
